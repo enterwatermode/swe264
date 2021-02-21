c2b2a9330999c934a8c3ea95808dd1437984b441
Hanbing Wang <hanbingwang@Hanbings-Mac-mini.local>
2021-02-21 13:23:41 -0800

    add Book class

---
+++ Kingdle/app/src/main/java/com/example/kingdle/Book.java
@@ -0,0 +1,51 @@
+package com.example.kingdle;
+
+public class Book {
+    //	• Cover image
+    //	• ISBN
+    //	• Author
+    //	• Pages
+    //	• Ratings
+    //Simple description
+
+    private String title;
+    private String author;
+    private Float rating;
+    private String description;
+    private String isbn;
+    private String img_path;
+    
+    public Book(String _title,
+                String _author,
+                Float _rating,
+                String _description,
+                String _isbn,
+                String _img_path) {
+        this.title = _title;
+        this.author = _author;
+        this.rating = _rating;
+        this.description = _description;
+        this.isbn = _isbn;
+        this.img_path = _img_path;
+    }
+    public String get_title () {
+        return title;
+    }
+    public String get_author () {
+        return author;
+    }
+    public Float get_rating () {
+        return rating;
+    }
+    public String get_description () {
+        return description;
+    }
+    public String get_isbn () {
+        return isbn;
+    }
+    public String get_img_path () {
+        return img_path;
+    }
+
+
+}
